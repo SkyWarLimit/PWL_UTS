ff343378b7519611a46fbf76643b0c0173443c2c
SkyWarLimit <ihsanbinrahmatw@gmail.com>
2026-04-21 11:30:29 +0700

    make model all menu

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Kategori extends Model
 {
     //
+    protected $table = 'm_kategori';
+    protected $primaryKey = 'kategori_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'kategori_kode',
+        'kategori_nama',
+    ];
+
+    public function barang()
+    {
+        return $this->hasMany(Barang::class, 'kategori_id', 'kategori_id');
+    }
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     //
+    protected $table = 'm_supplier';
+    protected $primaryKey = 'supplier_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'supplier_kode',
+        'supplier_nama',
+        'supplier_alamat',
+    ];
+
+    public function stok()
+    {
+        return $this->hasMany(Stok::class, 'supplier_id', 'supplier_id');
+    }
 }
